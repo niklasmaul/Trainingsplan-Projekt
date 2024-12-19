7a62717f7a07c80193fa05f414f88108e6ffde28
niklasmaul <niklas2018@icloud.com>
2024-12-19 12:05:11 +0100

    GUI

---
+++ src/main/java/de/projekt/main/Main.java
@@ -0,0 +1,11 @@
+package de.projekt.main;
+
+import de.projekt.swing.Trainingsplan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Trainingsplan();
+    }
+
+}
--- src/main/java/org/example/Trainingsplan.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import javax.swing.*;
-
-public class Trainingsplan {
-    private JTextField textField1;
-    private JComboBox comboBox1;
-}
